c673e9f502e5d36a4c8e85d3970dda843ff9c61e
Amiromar1 <Amiromar1@users.noreply.github.com>
2025-05-20 20:35:19 +0000

    deploy: f4a634974da88e176780283734d291db7bf2c767

--- src/config/emailjs.ts
+++
@@ -1,5 +0,0 @@
-export const EMAILJS_CONFIG = {
-  SERVICE_ID: "service_y7c1pss", // Add your EmailJS service ID
-  TEMPLATE_ID: "template_ljwyhij", // Add your EmailJS template ID
-  PUBLIC_KEY: "0rLtReLOf44r9N4ae", // Add your EmailJS public key
-} 
--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-// Use createRoot API with concurrent features
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
-)
